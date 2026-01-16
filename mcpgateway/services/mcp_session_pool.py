7fddbabb14923c0a86061e6f0b6d8043dadfa95c
Madhav Kandukuri <madhav165@gmail.com>
2026-01-16 15:10:21 +0530

    Add x-mcp-session-id to default identity headers Signed-off-by: Madhav Kandukuri <madhav165@gmail.com>

--- mcpgateway/services/mcp_session_pool.py
+++ mcpgateway/services/mcp_session_pool.py
@@ -184,6 +184,7 @@ class MCPSessionPool:  # pylint: disable=too-many-instance-attributes
             "x-user-id",
             "x-api-key",
             "cookie",
+            "x-mcp-session-id",
         ]
     )
 
